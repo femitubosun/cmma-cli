2f3f587a3c20c9e27b7417cc9e574fe4f5ce9508
Femi Ayo-Tubosun <thefemiolatubosun@gmail.com>
2023-06-02 22:39:26 +0100

    feat: update system artifacts

--- cmma/Helpers/SystemMessages/SystemMessageFunction.ts
+++ cmma/Helpers/SystemMessages/SystemMessageFunction.ts
@@ -1,8 +1,31 @@
-export function FOUND_NUMBER_OF_ENTITY_X_IN_ENTITY_Y(foundNumberOfEntityXInEntityYOptions: {
-  numberOfFoundEntities: number
-  foundEntity: string
-  sourceEntity: string
+export function FOUND_NUMBER_OF_ENTITY_ON_MAP_BUT_NOT_ON_DISK(foundNumberOfEntityXInEntityYOptions: {
+  entityCount: number
+  entityLabel: string
 }) {
-  const { numberOfFoundEntities, foundEntity, sourceEntity } = foundNumberOfEntityXInEntityYOptions
-  return `Found ${numberOfFoundEntities} ${foundEntity}(s) in ${sourceEntity}`
+  const { entityLabel, entityCount } = foundNumberOfEntityXInEntityYOptions
+  return `Found ${entityCount} ${entityLabel}(s) on Project Map but not on Disk}`
+}
+
+export function FOUND_NUMBER_OF_ENTITIES_ON_DISK_BUT_NOT_ON_MAP(foundNumberOfEntityXInEntityYOptions: {
+  entityCount: number
+  entityLabel: string
+}) {
+  const { entityLabel, entityCount } = foundNumberOfEntityXInEntityYOptions
+  return `Found ${entityCount} ${entityLabel}(s) on Disk but not on Project Map}`
+}
+
+export function ENTITY_PRUNED_FROM_PROJECT_MAP(entityPrunedFromProjectMapOptions: {
+  entityLabel: string
+  entityCount: number
+}) {
+  const { entityLabel, entityCount } = entityPrunedFromProjectMapOptions
+  return `${entityCount} ${entityLabel}(s) pruned from Project Map`
+}
+
+export function ENTITY_ADDED_TO_PROJECT_MAP(entityPrunedFromProjectMapOptions: {
+  entityLabel: string
+  entityCount: number
+}) {
+  const { entityLabel, entityCount } = entityPrunedFromProjectMapOptions
+  return `${entityCount} ${entityLabel}(s) added to Project Map`
 }
--- commands/Cmma/Config/Update.ts
+++ commands/Cmma/Config/Update.ts
@@ -3,10 +3,23 @@ import CmmaProjectMapActions from '../../../cmma/Actions/CmmaProjectMapActions'
 import CmmaFileActions from '../../../cmma/Actions/CmmaFileActions'
 import CmmaNodePath from '../../../cmma/Models/CmmaNodePath'
 import differenceOfArrays from '../../../cmma/Helpers/Utils/symettericDifferenceOfArrays'
-import { PRUNING } from '../../../cmma/Helpers/SystemMessages'
+import {
+  PRUNING_PROJECT_MAP,
+  UPDATING_PROJECT_MAP,
+} from '../../../cmma/Helpers/SystemMessages/SystemMessages'
 import CmmaConfigurationActions from '../../../cmma/Actions/CmmaConfigurationActions'
 import CmmaContextActions from '../../../cmma/Actions/CmmaContextActions'
 import CmmaSystemActions from '../../../cmma/Actions/CmmaSystemActions'
+import CmmaSystem from '../../../cmma/Models/CmmaSystem'
+import CmmaContext from '../../../cmma/Models/CmmaContext'
+import {
+  ENTITY_ADDED_TO_PROJECT_MAP,
+  ENTITY_PRUNED_FROM_PROJECT_MAP,
+  FOUND_NUMBER_OF_ENTITIES_ON_DISK_BUT_NOT_ON_MAP,
+  FOUND_NUMBER_OF_ENTITY_ON_MAP_BUT_NOT_ON_DISK,
+} from '../../../cmma/Helpers/SystemMessages/SystemMessageFunction'
+import CmmaModuleActions from '../../../cmma/Actions/CmmaModuleActions'
+import CmmaArtifactActions from '../../../cmma/Actions/CmmaArtifactActions'
 
 export default class ConfigUpdate extends BaseCmmaCommand {
   /*
@@ -40,64 +53,79 @@ export default class ConfigUpdate extends BaseCmmaCommand {
   |--------------------------------------------------------------------------------
   |
   */
-  public updateProjectContexts() {
-    /**
-     * Check for New Contexts in Project Directory
-     */
+  private updateProjectContexts() {
+    this.logger.info('Updating Project Contexts')
+
     const directoryContexts = CmmaFileActions.listSubDirsInDir(this.projectRootPath)
     let projectContexts = CmmaProjectMapActions.listContextsInProject(this.projectMap)
 
     const contextsOnDiskButNotOnMap = differenceOfArrays(directoryContexts, projectContexts)
 
     this.logger.info(
-      `${contextsOnDiskButNotOnMap.length} new Context(s) found: ${contextsOnDiskButNotOnMap} Updating...`
+      `${FOUND_NUMBER_OF_ENTITIES_ON_DISK_BUT_NOT_ON_MAP({
+        entityLabel: 'Context',
+        entityCount: contextsOnDiskButNotOnMap.length,
+      })}: ${contextsOnDiskButNotOnMap} ${UPDATING_PROJECT_MAP}`
     )
 
-    contextsOnDiskButNotOnMap.forEach((context) => {
-      const normalizedLabel = CmmaConfigurationActions.normalizeProjectIdentifier({
-        configObject: this.PROJECT_CONFIG,
-        identifier: String(context),
-      })
-
+    contextsOnDiskButNotOnMap.forEach((contextLabel) => {
       const defaultContext = CmmaContextActions.blankContext
-      defaultContext.contextLabel = normalizedLabel
+      defaultContext.contextLabel = contextLabel
 
       CmmaProjectMapActions.addContextToProject({
         context: defaultContext,
-        contextLabel: normalizedLabel,
+        contextLabel: contextLabel,
         projectMap: this.projectMap,
       })
     })
 
-    this.logger.info(`${contextsOnDiskButNotOnMap.length} Context(s) added to the Project Map`)
+    this.logger.info(
+      `${ENTITY_ADDED_TO_PROJECT_MAP({
+        entityCount: contextsOnDiskButNotOnMap.length,
+        entityLabel: 'Context',
+      })}: ${contextsOnDiskButNotOnMap} `
+    )
 
     // Contexts in Project Map that are not on disk
 
     projectContexts = CmmaProjectMapActions.listContextsInProject(this.projectMap)
 
-    const contextsInMapNotOnDisk = differenceOfArrays(projectContexts, directoryContexts)
+    const contextsOnMapNotOnDisk = differenceOfArrays(projectContexts, directoryContexts)
 
     this.logger.info(
-      `${contextsInMapNotOnDisk.length} context(s) found in Project Map but not on disk: ${contextsOnDiskButNotOnMap} ${PRUNING}`
+      `${FOUND_NUMBER_OF_ENTITY_ON_MAP_BUT_NOT_ON_DISK({
+        entityLabel: 'Context',
+        entityCount: contextsOnMapNotOnDisk.length,
+      })}: ${contextsOnDiskButNotOnMap}. ${PRUNING_PROJECT_MAP}`
     )
 
-    contextsInMapNotOnDisk.forEach((context) => {
+    contextsOnMapNotOnDisk.forEach((context) => {
       CmmaProjectMapActions.deleteContextByLabel({
         contextLabel: context,
         projectMap: this.projectMap,
       })
     })
 
-    this.logger.info(`${contextsInMapNotOnDisk.length} Context(s) pruned from the Project Map`)
+    this.logger.info(
+      `${ENTITY_PRUNED_FROM_PROJECT_MAP({
+        entityLabel: 'Context',
+        entityCount: contextsOnMapNotOnDisk.length,
+      })}`
+    )
   }
 
-  /**
-   * Update Project Systems
-   */
-  public updateProjectSystems() {
-    const contextLables = CmmaProjectMapActions.listContextsInProject(this.projectMap)
+  /*
+  |--------------------------------------------------------------------------------
+  | Update Project Systems
+  |--------------------------------------------------------------------------------
+  |
+  */
+  private updateProjectSystems() {
+    this.logger.info('Updating Project Systems')
 
-    for (const contextLabel of contextLables) {
+    const contextLabels = CmmaProjectMapActions.listContextsInProject(this.projectMap)
+
+    for (const contextLabel of contextLabels) {
       const contextMap = CmmaProjectMapActions.getContextMapByLabel({
         projectMap: this.projectMap,
         contextLabel,
@@ -106,7 +134,7 @@ export default class ConfigUpdate extends BaseCmmaCommand {
       const systemsOnMap = CmmaContextActions.listSystemsInContext(contextMap)
 
       const contextPath = new CmmaNodePath(this.PROJECT_CONFIG)
-        .drawPath()
+        .buildPath()
         .toContext(contextLabel)
         .getAbsoluteOsPath(this.application.appRoot)
 
@@ -115,7 +143,10 @@ export default class ConfigUpdate extends BaseCmmaCommand {
       const systemsOnDiskButNotInProject = differenceOfArrays(systemsOnDisk, systemsOnMap)
 
       this.logger.info(
-        `${systemsOnDiskButNotInProject.length} new System(s) found in ${contextLabel} Context: ${systemsOnDiskButNotInProject} Updating Project Map...`
+        `${FOUND_NUMBER_OF_ENTITIES_ON_DISK_BUT_NOT_ON_MAP({
+          entityCount: systemsOnDiskButNotInProject.length,
+          entityLabel: 'System',
+        })}: ${systemsOnDiskButNotInProject}. ${UPDATING_PROJECT_MAP}`
       )
 
       systemsOnDiskButNotInProject.forEach((systemLabel) => {
@@ -134,12 +165,22 @@ export default class ConfigUpdate extends BaseCmmaCommand {
         })
       })
 
+      this.logger.success(
+        `${ENTITY_ADDED_TO_PROJECT_MAP({
+          entityCount: systemsOnDiskButNotInProject.length,
+          entityLabel: 'System',
+        })}: ${systemsOnDiskButNotInProject} `
+      )
+
       const updatedSystems = CmmaContextActions.listSystemsInContext(contextMap)
 
       const systemsInProjectButNotOnDisk = differenceOfArrays(updatedSystems, systemsOnDisk)
 
       this.logger.info(
-        `${systemsInProjectButNotOnDisk.length} System(s) found in Project Map, but not on Disk Context: ${systemsInProjectButNotOnDisk} ${PRUNING}`
+        `${FOUND_NUMBER_OF_ENTITIES_ON_DISK_BUT_NOT_ON_MAP({
+          entityCount: systemsInProjectButNotOnDisk.length,
+          entityLabel: 'System',
+        })}: ${systemsInProjectButNotOnDisk}. ${UPDATING_PROJECT_MAP}`
       )
 
       systemsInProjectButNotOnDisk.forEach((systemLabel) => {
@@ -149,14 +190,275 @@ export default class ConfigUpdate extends BaseCmmaCommand {
         })
       })
 
-      this.logger.info(`${systemsInProjectButNotOnDisk.length} System(s) pruned from Project Map`)
+      this.logger.success(
+        `${ENTITY_PRUNED_FROM_PROJECT_MAP({
+          entityLabel: 'System',
+          entityCount: systemsInProjectButNotOnDisk.length,
+        })}`
+      )
     }
   }
 
-  /**
-   * Update Project Modules
-   */
-  public updateProjectModules() {}
+  /*
+  |--------------------------------------------------------------------------------
+  | Update Project Modules
+  |--------------------------------------------------------------------------------
+  |
+  */
+  private updateProjectModules() {
+    const contextLabels = CmmaProjectMapActions.listContextsInProject(this.projectMap)
+
+    for (let contextLabel of contextLabels) {
+      const contextMap = CmmaProjectMapActions.getContextMapByLabel({
+        contextLabel,
+        projectMap: this.projectMap,
+      })
+
+      this.updateContextModules(contextMap)
+    }
+  }
+
+  /*
+  |--------------------------------------------------------------------------------
+  | Update Context Modules
+  |--------------------------------------------------------------------------------
+  |
+  */
+  private updateContextModules(contextMap: CmmaContext) {
+    this.logger.info('Updating Project Modules')
+    const contextSystems = CmmaContextActions.listSystemsInContext(contextMap)
+
+    for (let systemLabel of contextSystems) {
+      const systemMap = CmmaContextActions.getContextSystemMapByLabel({
+        systemLabel,
+        contextMap,
+      })
+
+      this.updateSystemModules({
+        systemMap,
+        contextMap,
+      })
+    }
+  }
+
+  /*
+  |--------------------------------------------------------------------------------
+  | Update System Modules
+  |--------------------------------------------------------------------------------
+  |
+  */
+  private updateSystemModules(updateSystemModulesOptions: {
+    contextMap: CmmaContext
+    systemMap: CmmaSystem
+  }) {
+    const { contextMap, systemMap } = updateSystemModulesOptions
+
+    const moduleDir = CmmaConfigurationActions.whatIsDefaultCreateModuleDirIn(
+      this.PROJECT_CONFIG
+    )[0]
+
+    const moduleRootDirPath = new CmmaNodePath(this.PROJECT_CONFIG)
+      .buildPath()
+      .toContext(contextMap.contextLabel)
+      .toSystem(systemMap.systemLabel)
+      .toArtifactsDir(moduleDir)
+      .getAbsoluteOsPath(this.application.appRoot)
+
+    const diskModules = CmmaFileActions.listSubDirsInDir(moduleRootDirPath)
+
+    // Add Modules on Disk to Project Map
+    if (CmmaFileActions.doesPathExist(moduleRootDirPath)) {
+      const mapModules = CmmaSystemActions.listModulesInSystem(systemMap)
+
+      const modulesOnDiskButNotOnMap = differenceOfArrays(diskModules, mapModules)
+
+      this.logger.info(
+        `${FOUND_NUMBER_OF_ENTITIES_ON_DISK_BUT_NOT_ON_MAP({
+          entityLabel: 'Module',
+          entityCount: modulesOnDiskButNotOnMap.length,
+        })}: ${modulesOnDiskButNotOnMap} ${UPDATING_PROJECT_MAP}`
+      )
+
+      // Add Modules to System
+      modulesOnDiskButNotOnMap.forEach((moduleLabel) => {
+        const defaultModule = CmmaModuleActions.blankModuleMap
+        defaultModule.moduleLabel = moduleLabel
+
+        CmmaSystemActions.addModuleToSystem({ moduleLabel, systemMap, module: defaultModule })
+      })
+
+      //   Add Module Artifacts to modules
+      modulesOnDiskButNotOnMap.forEach((moduleLabel) => {
+        const moduleMap = CmmaSystemActions.getModuleMapByLabel({
+          moduleLabel,
+          systemMap,
+        })
+
+        const defaultModuleDirs = CmmaConfigurationActions.whatIsDefaultCreateModuleDirIn(
+          this.PROJECT_CONFIG
+        )
+
+        for (let moduleRootDir of defaultModuleDirs) {
+          const moduleArtifactRoute = new CmmaNodePath(this.PROJECT_CONFIG)
+            .buildPath()
+            .toContext(contextMap.contextLabel)
+            .toSystem(systemMap.systemLabel)
+            .toArtifactsDir(moduleRootDir)
+            .toModule(moduleMap.moduleLabel)
+            .getAbsoluteOsPath(this.application.appRoot)
+
+          const filesInModuleDir = CmmaFileActions.listFilesInDir(moduleArtifactRoute)
+
+          filesInModuleDir.forEach((file) => {
+            let artifact = CmmaArtifactActions.blankArtifact
+
+            // get extension
+            artifact = file.split('.')[0]
+
+            CmmaModuleActions.addArtifactToModule({
+              artifact,
+              artifactDirLabel: moduleRootDir,
+              moduleMap,
+            })
+          })
+        }
+      })
+
+      this.logger.success(
+        `${ENTITY_ADDED_TO_PROJECT_MAP({
+          entityCount: modulesOnDiskButNotOnMap.length,
+          entityLabel: 'Module',
+        })}: ${modulesOnDiskButNotOnMap}`
+      )
+    }
+
+    // Prune Project Map
+    const modulesInSystemMap = CmmaSystemActions.listModulesInSystem(systemMap)
+    const modulesOnMapButNotOnDisk = differenceOfArrays(modulesInSystemMap, diskModules)
+
+    this.logger.info(
+      `${FOUND_NUMBER_OF_ENTITY_ON_MAP_BUT_NOT_ON_DISK({
+        entityCount: modulesOnMapButNotOnDisk.length,
+        entityLabel: 'Module',
+      })}: ${modulesOnMapButNotOnDisk} ${PRUNING_PROJECT_MAP}`
+    )
+
+    modulesOnMapButNotOnDisk.forEach((moduleLabel) => {
+      CmmaSystemActions.deleteModuleByLabel({
+        moduleLabel,
+        systemMap,
+      })
+    })
+
+    this.logger.success(
+      `${ENTITY_PRUNED_FROM_PROJECT_MAP({
+        entityCount: modulesOnMapButNotOnDisk.length,
+        entityLabel: 'Module',
+      })}`
+    )
+  }
+
+  /*
+  |--------------------------------------------------------------------------------
+  | Update Project Artifacts
+  |--------------------------------------------------------------------------------
+  |
+  */
+
+  private updateProjectArtifacts() {
+    this.logger.info('Updating Project Artifacts')
+    const contextLabels = CmmaProjectMapActions.listContextsInProject(this.projectMap)
+
+    contextLabels.forEach((contextLabel) => {
+      const contextMap = CmmaProjectMapActions.getContextMapByLabel({
+        contextLabel,
+        projectMap: this.projectMap,
+      })
+
+      this.updateContextArtifacts(contextMap)
+    })
+  }
+
+  /*
+  |--------------------------------------------------------------------------------
+  | Update Context Artifacts
+  |--------------------------------------------------------------------------------
+  |
+  */
+  private updateContextArtifacts(contextMap: CmmaContext) {
+    const systemLabels = CmmaContextActions.listSystemsInContext(contextMap)
+
+    systemLabels.forEach((systemLabel) => {
+      const systemMap = CmmaContextActions.getContextSystemMapByLabel({
+        systemLabel,
+        contextMap,
+      })
+
+      this.updateSystemArtifacts({
+        systemMap,
+        contextMap,
+      })
+    })
+  }
+
+  /*
+  |--------------------------------------------------------------------------------
+  | Update System Artifacts
+  |--------------------------------------------------------------------------------
+  |
+  */
+  private updateSystemArtifacts(updateSystemArtifactsOptions: {
+    contextMap: CmmaContext
+    systemMap: CmmaSystem
+  }) {
+    const { contextMap, systemMap } = updateSystemArtifactsOptions
+
+    const defaultArtifactsDir = CmmaConfigurationActions.whatIsDefaultSystemArtifactDirs(
+      this.PROJECT_CONFIG
+    )
+
+    for (let artifactsDir of defaultArtifactsDir) {
+      const systemArtifactsDir = new CmmaNodePath(this.PROJECT_CONFIG)
+        .buildPath()
+        .toContext(contextMap.contextLabel)
+        .toSystem(systemMap.systemLabel)
+        .toArtifactsDir(artifactsDir)
+        .getAbsoluteOsPath(this.application.appRoot)
+
+      const artifactsOnDisk = CmmaFileActions.listFilesInDir(systemArtifactsDir)
+      const artifactsOnMap = CmmaSystemActions.listSystemArtifactsByGroupLabel({
+        systemMap,
+        artifactDir: artifactsDir,
+      })
+
+      const artifactsOnDiskButNotOnMap = differenceOfArrays(artifactsOnDisk, artifactsOnMap)
+
+      this.logger.info(
+        `${FOUND_NUMBER_OF_ENTITIES_ON_DISK_BUT_NOT_ON_MAP({
+          entityLabel: artifactsDir,
+          entityCount: artifactsOnDiskButNotOnMap.length,
+        })}: ${artifactsOnDiskButNotOnMap} ${UPDATING_PROJECT_MAP}`
+      )
+
+      artifactsOnDiskButNotOnMap.forEach((artifactLabel) => {
+        let artifact = CmmaArtifactActions.blankArtifact
+        artifact = artifactLabel
+
+        CmmaSystemActions.addArtifactToArtifactGroup({
+          artifact,
+          systemMap,
+          artifactsDir: artifactsDir,
+        })
+      })
+
+      this.logger.success(
+        `${ENTITY_ADDED_TO_PROJECT_MAP({
+          entityLabel: artifactsDir,
+          entityCount: artifactsOnDiskButNotOnMap.length,
+        })}`
+      )
+    }
+  }
 
   /*
   |--------------------------------------------------------------------------------
@@ -168,12 +470,16 @@ export default class ConfigUpdate extends BaseCmmaCommand {
     await this.ensureConfigFileExistsCommandStep()
 
     this.projectRootPath = new CmmaNodePath(this.PROJECT_CONFIG)
-      .drawPath()
+      .buildPath()
       .getAbsoluteOsPath(this.application.appRoot)
 
     this.updateProjectContexts()
 
     this.updateProjectSystems()
+
+    this.updateProjectModules()
+
+    this.updateProjectArtifacts()
 
     await this.finishCmmaCommand()
   }
