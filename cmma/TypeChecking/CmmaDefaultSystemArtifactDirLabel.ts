f0f2a2b4df0e032c8e060b838148eea3edfb4766
Femi Ayo-Tubosun <thefemiolatubosun@gmail.com>
2023-06-04 20:48:07 +0100

    refactor: remove DefaultSystemArtifactDirLabel type

--- cmma/TypeChecking/CmmaDefaultSystemArtifactDirLabel.ts
+++
@@ -1,9 +0,0 @@
-type CmmaDefaultSystemArtifactDirLabel =
-  | 'views'
-  | 'models'
-  | 'actions'
-  | 'migrations'
-  | 'typechecking'
-  | 'routes'
-
-export default CmmaDefaultSystemArtifactDirLabel
